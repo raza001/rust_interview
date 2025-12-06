bf7527e7cdea932682589dbc4b04e2d7960a8033
ARAZA <mohd.raza@1finance.org.in>
2025-12-06 22:20:10 +0530

    add dist file

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react()],
-      base: "/rust_interview/", 
+      plugins: [react()], 
       // define: {
       //   'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       //   'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
